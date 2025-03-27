7ddf9a547fe1a6861cbcf3813a75e88da84628db
AlexNikic <nikicalex@gmail.com>
2025-03-27 03:38:48 +0000

    alexnet template now trains

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torchvision import datasets, transforms, models
 from torch.utils.data import DataLoader
-import os
+from tqdm import tqdm
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -17,33 +17,56 @@ transform = transforms.Compose([
 ])
 
 # TODO: change these to test/train folders when done
-train_dataset = datasets.ImageFolder("images", transform=transform)
-test_dataset = datasets.ImageFolder("images", transform=transform)
+train_dataset = datasets.ImageFolder("images2", transform=transform)
+test_dataset = datasets.ImageFolder("images2", transform=transform)
 
 # Load datasets into data loaders
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-
 model = models.alexnet(pretrained=True)
 model.to(device)
-print(model)
 
 # Modify the final fully connected layer (classifier) to match the number of classes in the Oxford-IIIT Pet Dataset (37 classes)
 num_ftrs = model.classifier[6].in_features  # Get the number of input features to the last fully connected layer
 model.classifier[6] = nn.Linear(num_ftrs, len(train_dataset.classes))  # Update to match number of classes
 
 
+# Training loop
+def train_model(model, num_epochs=1, lr=1e-3):
+    print("***Training***")
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+        for inputs, labels in tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}"):
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward() 
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(train_loader):.4f}")
+
+
 # Function to evaluate the model
 def evaluate_model(model, test_loader):
+    print("***Evaluating***")
+    
     model.eval()
     running_corrects = 0
     total = 0
     
     with torch.no_grad():
-        for inputs, labels in test_loader:
-            inputs.to(device)
-            labels.to(device)
+        for inputs, labels in tqdm(test_loader, desc=f"Evaluating"):
+            inputs, labels = inputs.to(device), labels.to(device)
 
             # Forward pass
             outputs = model(inputs)
@@ -52,10 +75,17 @@ def evaluate_model(model, test_loader):
             total += labels.size(0)
             running_corrects += (predicted == labels).sum().item()
 
-            print(running_corrects / total)
+            print(f"Progress: {total}/7390")
 
     accuracy = running_corrects / total
-    print(f"Test Accuracy: {accuracy:.4f}")
+    print(f"Accuracy: {accuracy:.4f}")
+
+
+# Train the model
+train_model(model, num_epochs=1)
 
 # Evaluate the model
 evaluate_model(model, test_loader)
+
+# Save the model
+torch.save(model.state_dict(), "model_weights.pth")
